ae45df6c1c243802c129af797f9dc4c04ceecb20
orisimh <zori1232@gmail.com>
2020-11-17 00:58:26 +0200

    hashtag function percent function tag function
    search engine - methos to add all files.

--- parser_module.py
+++ parser_module.py
@@ -1,6 +1,8 @@
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from document import Document
+import re
+from string import punctuation
 
 
 class Parse:
@@ -8,21 +10,87 @@ class Parse:
     def __init__(self):
         self.stop_words = stopwords.words('english')
 
-    def apply_rules(self, tokens_list):
-        tokens_list = self.hashtag(tokens_list)
-        tokens_list = self.tags(tokens_list)
-        tokens_list = self.percents(tokens_list)
-        tokens_list = self.numbers(tokens_list)
-        tokens_list = self.urls(tokens_list)
 
-        return tokens_list
 
-    def hastag(self, tokens_list):
+    # caring off tags
+    def tags(self,text):
 
-        """
-        Do work
-        """
-        return tokens_list
+        tags=[text[i+1] for i in range(0,len(text)) if text[i] == '@']
+        print (tags)
+        return tags
+    print (tags)
+    # caring off percent
+    def percentage(self,text):
+        percent = []
+        for prc in text:
+            cnt = cnt + 1
+            if prc in ('percent', 'percentage'):
+                percent.append("{}%".format(text[cnt - 2]))
+            if '%' in prc:
+                percent.append(prc)
+
+        print(percent)
+
+        return percent
+
+    # caring off hashtag
+    def hashtag(self,text):
+        #print(text)
+        #hashtaglist = [w for w in text if w[0] == '#']
+        terms = []
+        #print (hashtaglist)
+        trms=[]
+        upcases = []
+
+        for i in range(0, len(text) - 1):
+
+            if (text[i] == '#'):
+                curren_hashtag = text[i + 1]
+                if (curren_hashtag[
+                    0].isupper() and curren_hashtag != curren_hashtag.isupper()):  # words that seperated by uppercase like #StayAtHome
+                    upcases.append("#{}".format(text[i + 1].lower()))
+                    for w1 in re.findall('[A-Z][^A-Z]*', text[i + 1]):
+                        upcases.append(w1.lower())
+                flag = 'true'
+                sgn = 'true'
+                if (text[i + 1][0].islower() and text[i + 1] != text[
+                    i + 1].islower()):  # words that starts with lowercase like #stayAtHome
+                    cnt1 = 0
+                    indx = 0
+                    for j in curren_hashtag:
+                        if (j.isupper() and sgn == 'true'):
+                            indx = cnt1
+                            sgn = 'false'
+                        cnt1 = cnt1 + 1
+                    if (sgn == 'false'):
+                        terms.append(text[i + 1][0:indx])
+                        for k in re.findall('[A-Z][^A-Z]*', text[i + 1][indx:]):
+                            terms.append(k.lower())
+                        terms.append("#{}".format(text[i + 1].lower()))
+
+                for j in text[i + 1]:  # words that seperated by puncutation #stay_at_home
+                    if j in punctuation and j not in ['@', '#']:
+                        for k in re.compile(r'[\s{}]+'.format(re.escape(punctuation))).split(text[i + 1]):
+                            terms.append(k.lower())
+                        terms.append("#{}".format(text[i + 1].lower()))
+                        break
+
+
+          #for i in hashtaglist:
+        print(terms)
+
+
+        '''''''''
+        hashtaglist = [w for w in text if w[0] == '#']
+
+        #upper case dispute from the words
+        upcases = [[re.findall('[A-Z][^A-Z]*',w[1:]),w] for w in text if w[1].isupper()]
+
+        #punctation dispute from the words
+        pctionltrs = [[re.findall(punctuation,w[1:]),w] for w in text if p in w  for p in punctuation]
+        '''''''''
+
+        return terms + upcases
 
 
     def parse_sentence(self, text):
@@ -31,8 +99,13 @@ class Parse:
         :param text:
         :return:
         """
+
         text_tokens = word_tokenize(text)
-        text_tokens_without_stopwords = [w.lower() for w in text_tokens if w not in self.stop_words]
+        text_tokens = self.hashtag(text_tokens)
+        text_tokens = self.tags(text_tokens)
+        text_tokens = self.percentage(text_tokens)
+        #text_tokens_without_stopwords = [w.lower() for w in text_tokens if w not in self.stop_words]
+        text_tokens_without_stopwords = [w for w in text_tokens if w not in self.stop_words]
         return text_tokens_without_stopwords
 
     def parse_doc(self, doc_as_list):
@@ -45,18 +118,18 @@ class Parse:
         tweet_date = doc_as_list[1]
         full_text = doc_as_list[2]
         url = doc_as_list[3]
+
         retweet_text = doc_as_list[4]
         retweet_url = doc_as_list[5]
         quote_text = doc_as_list[6]
         quote_url = doc_as_list[7]
         term_dict = {}
+
         tokenized_text = self.parse_sentence(full_text)
 
         doc_length = len(tokenized_text)  # after text operations.
 
-        tokenized_text_w_rules = self.aplly_rules(tokenized_text)
-
-        for term in tokenized_text_w_rules:
+        for term in tokenized_text:
             if term not in term_dict.keys():
                 term_dict[term] = 1
             else:
--- search_engine.py
+++ search_engine.py
@@ -4,39 +4,88 @@ from parser_module import Parse
 from indexer import Indexer
 from searcher import Searcher
 import utils
+from os import listdir
+from ziplnew import Zipf
+
+from os.path import isfile, join
+import  pandas as pd
 
 
 def run_engine():
     """
-
     :return:
     """
     number_of_documents = 0
 
-    config = ConfigClass()
+    config = ConfigClass(r"C:\Users\User\Desktop\project_reterive_info\Data")
+    #onlyfiles = [f for f in listdir("{}\{}".format(config.get__corpusPath(),f)) if isfile(join(config.get__corpusPath(), f) )]
+    onlyfiles = ["{}\{}".format(config.get__corpusPath(),f)  for f in listdir(config.get__corpusPath()) ]
+    #print(onlyfiles)
+
+
     r = ReadFile(corpus_path=config.get__corpusPath())
+    documents_list = []
     p = Parse()
     indexer = Indexer(config)
+    cnt=0
+    for file in onlyfiles:
 
-    documents_list = r.read_file(file_name='sample3.parquet')
+        if(cnt<2):
+         for f in listdir(file):
+          documents_list = documents_list + (r.read_file(file_name="{}\{}".format(file,f)))
+        else:
+            break
+    cnt = cnt + 1
+
+    print(documents_list[0:2])
+    '''''''''
+    for i in (documents_list[0]):
+       print(i[1][0])
+       ##for j in  i[1] :
+        ##print (j)
+    '''''''''
+
+
+    #pd.to_csv(documents_list['full_text'][0:1])
+#   '''''''''
     # Iterate over every document in the file
+
     for idx, document in enumerate(documents_list):
         # parse the document
+        #print(document)
         parsed_document = p.parse_doc(document)
         number_of_documents += 1
         # index the document data
         indexer.add_new_doc(parsed_document)
+
     print('Finished parsing and indexing. Starting to export files')
+
+    zp = Zipf(indexer.inverted_idx)
+    zp.parse_uplowchar()
 
     utils.save_obj(indexer.inverted_idx, "inverted_idx")
     utils.save_obj(indexer.postingDict, "posting")
 
 
+
 def load_index():
     print('Load inverted index')
     inverted_index = utils.load_obj("inverted_idx")
+
+
     return inverted_index
 
+newDict = { key:value for (key,value) in utils.load_obj("inverted_idx").items() if value> 1}
+
+
+
+'''''''''
+for  i in utils.load_obj("inverted_idx").keys():
+    if utils.load_obj("inverted_idx")[i] >1:
+        newdc[i] = utils.load_obj("inverted_idx")[i]
+
+print(newdc)
+'''''''''
 
 def search_and_rank_query(query, inverted_index, k):
     p = Parse()
@@ -46,7 +95,7 @@ def search_and_rank_query(query, inverted_index, k):
     ranked_docs = searcher.ranker.rank_relevant_doc(relevant_docs)
     return searcher.ranker.retrieve_top_k(ranked_docs, k)
 
-
+ #   '''''''''
 def main():
     run_engine()
     query = input("Please enter a query: ")
